5253b064b9b93505c0fd9b93bce49da36b029da8
mohamedarbaj348-cloud <mohamedarbaj348@gmail.com>
2026-05-04 12:14:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# IT Helpdesk System
+
+Simple Python project that simulates an IT support ticket system.
+
+## Features
+- Create tickets
+- Show tickets
+- Categorize problems (Network / PC / Software)
+
+## What I learned
+- Python basics
+- Problem solving
+- IT support logic
+
+## Author
+[Mohammed Aarbaj]
